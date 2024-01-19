694ddbbea87cc808b958c7f57abb0ef73f3ac79e
icanc <44480394+icanc0@users.noreply.github.com>
2024-01-19 12:03:22 -0500

    initial setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<style>
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+</style>
+
+
+<div class="framer-m6sp60" data-framer-name="Hero" name="Hero">
+    <div class="framer-1ughbhb" style="outline: none; display: flex; flex-direction: column; justify-content: flex-start; flex-shrink: 0; opacity: 1; transform: perspective(1200px) translateX(20px) translateY(0px) scale(1) rotate(0deg) rotateX(0deg) rotateY(0deg) translateZ(0px);" data-framer-component-type="RichTextContainer">
+        <p class="framer-text framer-styles-preset-1mdord3" data-styles-preset="JG4rYVHpN" style="--framer-text-color:var(--token-de8878a1-c3c9-4d8b-bcbd-ed3d3d9571a8, rgb(37, 41, 29))">VR/AR/XR · Spatial Computing</p>
+    </div>
+    <div class="framer-l5382l" style="outline: none; display: flex; flex-direction: column; justify-content: flex-start; flex-shrink: 0; opacity: 1; transform: perspective(1200px) translateX(30px) translateY(0px) scale(1) rotate(0deg) rotateX(0deg) rotateY(0deg) translateZ(0px);" data-framer-component-type="RichTextContainer">
+        <h1 class="framer-text framer-styles-preset-zcx1yu" data-styles-preset="KN1n4eozB" style="--framer-text-color:rgb(133, 133, 133)">
+            <span style="--framer-text-color:var(--token-de8878a1-c3c9-4d8b-bcbd-ed3d3d9571a8, rgb(37, 41, 29))" class="framer-text">The University of Waterloo's </span>
+            <span style="--framer-text-color:var(--token-be079bad-eabd-4234-8a56-5579c7057187, rgb(0, 0, 0))" class="framer-text"><br class="framer-text"></span>XR &amp; spatial computing <br class="framer-text">design team.
+        </h1>
+    </div>
+    <div class="framer-qn66lz-container" style="opacity: 1; transform: perspective(1200px) translateX(40px) translateY(0px) scale(1) rotate(0deg) rotateX(0deg) rotateY(0deg) translateZ(0px);">
+        <div style="display:contents">
+            <a class="framer-jFFbf framer-HmHau framer-1whgvtw framer-v-1whgvtw framer-1gdlr2s" data-border="true" data-framer-name="Primary" style="--border-bottom-width: 1px; --border-color: rgba(0, 0, 0, 0); --border-left-width: 1px; --border-right-width: 1px; --border-style: solid; --border-top-width: 1px; background-color: var(--token-de8878a1-c3c9-4d8b-bcbd-ed3d3d9571a8, rgb(37, 41, 29)); border-radius: 64px; opacity: 1;" href="./projects" tabindex="0">
+                <div class="framer-1jx58dg" style="outline: none; display: flex; flex-direction: column; justify-content: flex-start; flex-shrink: 0; --extracted-1eung3n: var(--token-e16847f2-b192-48e1-a890-7bbcce11cc6e, rgb(255, 255, 255)); --framer-paragraph-spacing: 0px; transform: none; opacity: 1;" data-framer-component-type="RichTextContainer">
+                    <h4 class="framer-text framer-styles-preset-1xo5nk6" data-styles-preset="bmgtGAtvT" style="--framer-text-color:var(--extracted-1eung3n, var(--token-e16847f2-b192-48e1-a890-7bbcce11cc6e, rgb(255, 255, 255)) )">Dive Deeper</h4>
+                </div>
+                <div class="framer-1u4mo67-container" style="transform: none; opacity: 1;">
+                    <div style="display:contents">
+                        <div class="framer-SBGIa framer-fdjns5 framer-v-1amdk14" data-framer-name="Variant 2" style="height: 100%; width: 100%; opacity: 1;" tabindex="0">
+                            <div data-framer-component-type="SVG" data-framer-name="Arrow" style="background-color: rgba(0, 0, 0, 0); image-rendering: pixelated; flex-shrink: 0; opacity: 1;" class="framer-6g0lxz">
+                                <div class="svgContainer" style="width:100%;height:100%;aspect-ratio:inherit">
+                                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="width: 100%; height: 100%">
+                                        <use href="#svg4164777255"></use>
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+</div>
