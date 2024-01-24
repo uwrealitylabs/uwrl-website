9c8360e2207046809a77a6e45ce864f11c4ce747
icanc <44480394+icanc0@users.noreply.github.com>
2024-01-23 19:25:42 -0500

    main page implementation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,22 @@
-<div>
+<div class="flex flex-col m-8 space-y-10">
+    <div class="text-sm ">
+        VR/AR/XR · Spatial Computing
+    </div>
+    <div class="ml-2">
+        <h1 class="font-light text-5xl">
+            <span >The University of Waterloo's </span>
+            <br class="framer-text">
+            <span >XR &amp; spatial computing</span>
+            <br class="framer-text">design team.
+        </h1>
+    </div>
+    <div >
+        <div >
+            <a href="/projects" class="round bg-black">
+                <div>
 
+                </div>
+            </a>
+        </div>
+    </div>
 </div>
