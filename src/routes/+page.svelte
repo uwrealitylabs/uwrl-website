510604fcb0de6576b10461792dd1de7e74f4448e
icanc <44480394+icanc0@users.noreply.github.com>
2024-01-29 12:14:59 -0500

    fixed front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,9 @@
     import ProjectCard from "$lib/ProjectCard.svelte";
 </script>
 
-<div class="m-8 flex flex-col">
-    <div class="flex flex-col space-y-10">
+<div class="m-8 flex flex-col ">
+
+    <div class="flex flex-col space-y-20 pb-10">
         <div class="text-sm ">
             VR/AR/XR · Spatial Computing
         </div>
@@ -17,10 +18,15 @@
             </h1>
         </div>
         <div >
-            <div >
-                <a href="/projects" class="round bg-black">
+            <div class="flex flex-row">
+                <a href="/projects" class="w-auto rounded-full bg-black px-6 py-4 flex flex-row space-x-3 group hover:bg-white hover:ring-2 ring-black ring-0">
+                    <div class="text-white group-hover:text-black">
+                        Dive deeper
+                    </div>
                     <div>
-
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="group-hover:stroke-black stroke-white w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                        </svg>
                     </div>
                 </a>
             </div>
