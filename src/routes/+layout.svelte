76635752c3c3bbdc773ca58fd8b24e2b8935f385
icanc <44480394+icanc0@users.noreply.github.com>
2024-02-09 19:08:48 -0500

    background fix + github actions update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,12 +10,13 @@
     import NavBar from "$lib/NavBar.svelte";
 	import Footer from '$lib/Footer.svelte';
 	import {DarkMode} from "$lib/States";
+	import { base } from '$app/paths';
 </script>
 
 
 <div class:dark={$DarkMode}>
     <NavBar></NavBar>
-	<div class="bg-repeat" style="background-image: url(/sketchTile.svg);">
+	<div class="bg-repeat" style="background-image: url({base}/sketchTile.svg);">
 		<slot></slot>
 	</div>
 	<Footer></Footer>
